d03233e49117d91c2ede2acdff5c410a71f0d25a
EstiMendelson <stm5706@gmail.com>
2022-02-06 15:52:14 +0200

    update url

--- routes/coupon.js
+++ routes/coupon.js
@@ -2,10 +2,10 @@ const express = require("express")
 const router = express.Router()
 const couponController = require("../controller/coupon")
 
-router.get('/getAll/:skip/:limit', couponController.getAll);
-router.post('/insertData', couponController.insertData);
-router.delete('/deleteMany/:list', couponController.deleteMany);
-router.delete('/deleteOne/:id', couponController.deleteOne);
+router.get('/get-all/:skip/:limit', couponController.getAll);
+router.post('/insert-data', couponController.insertData);
+router.delete('/delete-many/:list', couponController.deleteMany);
+router.delete('/delete-one/:id', couponController.deleteOne);
 router.put('/update/:id', couponController.update);
 
 module.exports = router
